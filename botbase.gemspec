6e71f2601ed22dc4d7ed9b52260cf63c09b03e78
Jeff Kowalski <jeff.kowalski@gmail.com>
2022-02-15 22:51:12 -0800

    add real content

--- botbase.gemspec
+++ botbase.gemspec
@@ -8,17 +8,17 @@ Gem::Specification.new do |spec|
   spec.authors = ["Jeff Kowalski"]
   spec.email = ["jeff.kowalski@gmail.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary = "Base definitions for common bots"
+  spec.description = "Defines with_rescue to retry on specific errors, RecorderBotBase - a class for bots that gather data for recording to influxdb, ScannerBotBase - a class for bots that inspect sources and create notifications"
+  spec.homepage = "https://github.com/jeffkowalski/botbase"
   spec.license = "MIT"
-  spec.required_ruby_version = ">= 2.6.0"
+  spec.required_ruby_version = ">= 3.0"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
+  spec.metadata["allowed_push_host"] = "https://example.com"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = spec.homepage
+  spec.metadata["changelog_uri"] = spec.homepage
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -31,8 +31,13 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
+  spec.add_dependency "thor", "~> 1.2"
+  spec.add_dependency "yaml", "~> 0.2"
+
+  spec.add_dependency "debug", "~> 1.4"
+  spec.add_dependency "method_source", "~> 1.0"
+  spec.add_dependency "pry", "~> 0.14"
+  #spec.add_dependency "pry_doc", "~> 1.3"
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
---
+++ lib/botbase/botbase.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+module Kernel
+  def with_rescue(exceptions, logger, retries: 5)
+    try = 0
+    begin
+      yield try
+    rescue *exceptions => e
+      try += 1
+      raise if try > retries
+
+      logger.info "caught error #{e.class}, retrying (#{try}/#{retries})..."
+      retry
+    end
+  end
+end
+
+class BotBase < Thor
+  attr_accessor :bot_name, :logger
+
+  no_commands do
+    def initialize(_args = [], _local_options = {}, _config = {})
+      @bot_name = self.class.to_s.downcase
+      super
+    end
+
+    private
+
+    def redirect_output
+      logfile_path = File.expand_path(File.join(Dir.home, '.log', "#{bot_name}.log"))
+      FileUtils.mkdir_p(File.dirbot_name(logfile_path), mode: 0o755)
+      FileUtils.touch logfile_path
+      File.chmod 0o644, logfile_path
+      $stdout.reopen logfile_path, 'a'
+      $stderr.reopen $stdout
+      $stdout.sync = $stderr.sync = true
+    end
+
+    protected
+
+    def setup_logger
+      redirect_output if options[:log]
+
+      @logger = Logger.new $stdout
+      @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
+    end
+
+    def load_credentials(source = bot_name)
+      credentials_path = File.join(Dir.home, '.credentials', "#{source}.yaml")
+      YAML.load_file credentials_path
+    end
+
+    def store_credentials(credentials, source = bot_name)
+      credentials_path = File.join(Dir.home, '.credentials', "#{source}.yaml")
+      File.open(credentials_path, 'w') { |file| file.write(credentials.to_yaml) }
+    end
+
+    no_commands do
+      def main
+        puts "please provide (override) #{self.class}::#{__method__}"
+      end
+    end
+  end
+
+  class_option :log,     type: :boolean, default: true, desc: 'log output to ~/.log/'
+  class_option :verbose, type: :boolean, aliases: '-v', desc: 'increase verbosity'
+end
+
+class ScannerBotBase < BotBase
+  desc 'scan', 'scan source and send notifications'
+  method_option :dry_run, type: :boolean, aliases: '-n', desc: "don't send notifications"
+  def scan
+    setup_logger
+
+    # do something
+    @logger.info 'starting'
+    # credentials = load_credentials
+    main
+    @logger.info 'done'
+  rescue StandardError => e
+    @logger.error e
+  end
+
+  default_task :scan
+end
+
+class RecorderBotBase < BotBase
+  desc 'record-status', 'record the current usage data to database'
+  method_option :dry_run, type: :boolean, aliases: '-n', desc: "don't write to database"
+  def record_status
+    setup_logger
+
+    @logger.info 'starting'
+    # credentials = load_credentials
+    main
+    @logger.info 'done'
+  rescue StandardError => e
+    @logger.error e
+  end
+
+  default_task :record_status
+end
